deb2ece5c089ad4c262c0a5acb8001d76671cf09
Samantha Burak <samantha.burak@gmail.com>
2021-06-25 16:32:18 -0400

    Added collection view layout

---
+++ Flix/Views/MovieCollectionCell.h
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionCell.h
+//  Flix
+//
+//  Created by samanthaburak on 6/25/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCollectionCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+@end
+
+NS_ASSUME_NONNULL_END
